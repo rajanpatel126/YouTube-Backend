1f65968e4b746047ee7e9e91e46899834665ddb3
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2024-01-19 14:19:04 +0530

    Project Completition.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({
 connectDB()
    .then(() => {
       app.listen(process.env.PORT || 8000, () => {
-         console.log(`Server is running at port: ${process.env.PORT}`);
+         console.log(`⚙️ Server is running at port: ${process.env.PORT}`);
       });
    })
    .catch((err) => {
